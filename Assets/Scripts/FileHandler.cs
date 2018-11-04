5e94b7e40a148be8f460b0aced13f24d1bad804a
Unknown <cavandiepen@gmail.com>
2018-11-04 16:48:19 +0100

    filehandler placeholder

---
+++ Assets/Scripts/FileHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileHandler : MonoBehaviour
+{
+    private string[,] map = { { "a", "b", "c" }, { "b", "b", "b" }, { "d", "c", "a" } };
+
+    public string[,] FileTo2DArray()
+    {
+        return map;
+    }
+}
